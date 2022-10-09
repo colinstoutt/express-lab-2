9ba67fb61039fb4a92c3eae78f08efff73bbdc3b
cps97 <colinstoutt@gmail.com>
2022-10-08 17:42:04 -0700

    add initial files and server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+});
