5c9a8344b60049bd2488ae27340f976f4029cdee
cps97 <colinstoutt@gmail.com>
2022-10-10 18:33:55 -0700

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ app.get("/", (req, res) => {
   res.send("HOME");
 });
 
-app.get("/greeting/:name", (req, res) => {
-  res.send(`Whats up, ${req.params.name}`);
+// app.get("/greeting/:name", (req, res) => {
+//   res.send(`Whats up, ${req.params.name}`);
+// });
+
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  res.send(
+    `You must tip $${
+      parseInt(req.params.total) * (parseInt(req.params.tipPercentage) / 100)
+    }`
+  );
 });
 
 const port = 3001;
